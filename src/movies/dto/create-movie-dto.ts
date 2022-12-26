cdca6ad0745f48069f77454e31b02ce7d800c27c
Kress50 <kondalovi@gmail.com>
2022-12-26 19:31:29 +0400

    added app module, movies module

--- src/movies/dto/create-movie-dto.ts
+++ src/movies/dto/create-movie-dto.ts
@@ -1,8 +1,4 @@
-import { IsString } from 'class-validator';
-import {
-  IsNumber,
-  IsOptional,
-} from 'class-validator/types/decorator/decorators';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class createMovieDto {
   @IsString()
