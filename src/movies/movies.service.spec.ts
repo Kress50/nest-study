c8f056e113a8a933750cabf72edc0abc9da2a3bd
Kress50 <kondalovi@gmail.com>
2022-12-26 20:31:06 +0400

    implemented unit testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
+import { serialize } from 'v8';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -14,5 +16,87 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genre: ['test'],
+      });
+      const result = service.getOne(1);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(1);
+    });
+    it('should throw 404', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a movie from an array', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genre: ['test'],
+      });
+      service.deleteOne(1);
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+      expect(result.length).toEqual(0);
+    });
+    it('should throw 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genre: ['test'],
+      });
+      const result = service.getAll().length;
+      expect(result).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genre: ['test'],
+      });
+      service.update(1, { title: 'Test Movie 2' });
+      const result = service.getOne(1);
+      expect(result.title).toEqual('Test Movie 2');
+    });
+    it('should throw 404', () => {
+      try {
+        service.update(222, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
